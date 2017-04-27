9271213fad416e16b7a4940b93426a528e33980a
veiji <victor_shinohara@hotmail.com>
2017-04-27 11:01:39 -0300

    Initial check-in of module MyContacts

---
+++ MyContacts/ContactDetailViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MyContacts
+{
+    public partial class ContactDetailViewController : UIViewController
+    {
+        public ContactDetailViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
